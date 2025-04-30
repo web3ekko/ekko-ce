dcae2240d776164e7530641b923841994b244c1d
Abraham Alaka <abraham@ekko.zone>
2025-04-30 10:32:34 +0100

    removing bento

--- bento/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-if [ -z "$AVAX_C_WEBSOCKET_URL" ]; then
-  echo "WARNING: AVAX_C_WEBSOCKET_URL is not set. Some processors may not function correctly."
-fi
-exec /app/benthos -c "$BENTHOS_CONFIG"
